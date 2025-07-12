1bb90f7ad145ef403a9878d72ea2de5aa1fbd007
medavenisiva <medavenishiva@gmail.com>
2025-07-12 12:47:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// Add scroll animations or interactions here in future
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const section = document.querySelector(this.getAttribute('href'));
-    section.scrollIntoView({ behavior: 'smooth' });
-  });
-});
+
