7a1137a947b44732fce3f8a02108c38f385945df
medavenisiva <medavenishiva@gmail.com>
2025-07-23 13:03:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// Simple contact form handler (for demo only)
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  document.getElementById("formMessage").textContent = "Thanks for your message!";
-  this.reset();
-});
+
